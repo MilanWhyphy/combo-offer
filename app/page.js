ae1d58087f7e89eae0182438e472118c1ba04f77
MilanWhyphy <milan.whyphy@gmail.com>
2025-04-28 13:37:28 +0530

    added menu items card

--- app/page.js
+++ app/page.js
@@ -87,42 +87,70 @@ export default function ComboOfferPage() {
     toast.success("Combo offer saved successfully");
   };
 
+  /** add to cart logic */
+  const handleAddToCart = (item) => {
+    console.log("item ", item);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.cardContainer}>
-        <Card className={styles.card}>
-          <div className={styles.cardContent}>
-            <div className={styles.left}>
-              <Typography className={styles.title}>All Products</Typography>
+    <>
+      <div className={styles.container}>
+        <div className={styles.cardContainer}>
+          <Card className={styles.card}>
+            <div className={styles.cardContent}>
+              <div className={styles.left}>
+                <Typography className={styles.title}>All Products</Typography>
 
-              <ProductList
-                products={Products}
-                selectedItems={selectedItems}
-                handleItemClick={handleItemClick}
-              />
-            </div>
-            <div className={styles.right}>
-              <CreateCombo
-                customerCanBuy={customerCanBuy}
-                comboPrice={comboPrice}
-                handleInputChange={handleInputChange}
-                selectedItems={selectedItems}
-                handleRemoveItem={handleRemoveItem}
-              />
-
-              <div className={styles.btnWrapper}>
-                <Button
-                  type="primary"
-                  label="Save"
-                  className={styles.btnSave}
-                  disabled={selectedItems.length === 0}
-                  onClick={handleSave}
+                <ProductList
+                  products={Products}
+                  selectedItems={selectedItems}
+                  handleItemClick={handleItemClick}
                 />
               </div>
+              <div className={styles.right}>
+                <CreateCombo
+                  customerCanBuy={customerCanBuy}
+                  comboPrice={comboPrice}
+                  handleInputChange={handleInputChange}
+                  selectedItems={selectedItems}
+                  handleRemoveItem={handleRemoveItem}
+                />
+
+                <div className={styles.btnWrapper}>
+                  <Button
+                    type="primary"
+                    label="Save"
+                    className={styles.btnSave}
+                    disabled={selectedItems.length === 0}
+                    onClick={handleSave}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        </div>
       </div>
-    </div>
+
+      <div className={styles.itemContainer}>
+        <div className={styles.cardContainer}>
+          <Card className={styles.card}>
+            <div className={styles.itemCardContent}>
+              <div className={styles.itemLeft}>
+                <Typography className={styles.title}>Menu Items</Typography>
+
+                <ProductList
+                  products={Products}
+                  selectedItems={selectedItems}
+                  handleItemClick={handleItemClick}
+                  isMenuItems={true}
+                  handleAddToCart={handleAddToCart}
+                />
+              </div>
+              <div className={styles.itemRight}>Create Cart Here</div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -4,31 +4,50 @@ import { Checkbox } from "antd";
 
 import styles from "./ProductList.module.css";
 
-const ProductList = ({ products, selectedItems, handleItemClick }) => {
+const ProductList = ({
+  isMenuItems = false,
+  products,
+  selectedItems,
+  handleItemClick,
+  handleAddToCart,
+}) => {
   return (
     <div className={styles.productList}>
-      {products.map((product) => (
-        <div
-          key={product.id + Date.now()}
-          className={styles.produtWrapper}
-          onClick={(e) => {
-            e.stopPropagation();
-            handleItemClick(product);
-          }}
-        >
-          <div className={styles.productInner}>
-            <Checkbox
-              checked={selectedItems.some((i) => i.id === product.id)}
-              onChange={(e) => {
-                e.stopPropagation();
-                handleItemClick(product);
-              }}
-            >
-              {product.name}
-            </Checkbox>
+      {products.map((product) =>
+        isMenuItems ? (
+          <div
+            key={product.id + Date.now()}
+            className={styles.produtWrapper}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleAddToCart(product);
+            }}
+          >
+            <div className={styles.productInner}>{product?.name}</div>
           </div>
-        </div>
-      ))}
+        ) : (
+          <div
+            key={product.id + Date.now()}
+            className={styles.produtWrapper}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleAddToCart(product);
+            }}
+          >
+            <div className={styles.productInner}>
+              <Checkbox
+                checked={selectedItems.some((i) => i.id === product.id)}
+                onChange={(e) => {
+                  e.stopPropagation();
+                  handleItemClick(product);
+                }}
+              >
+                {product.name}
+              </Checkbox>
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 };
